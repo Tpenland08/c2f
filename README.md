00429168127d512eb1e411981a05cb26574caa99
Teagan Penland <>
2021-11-04 18:37:39 +0000

    Added Fahrenheit to celsius conversion capabilities.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # c2f
-A celsius to fahrenheit converter written in C++.
+A celsius to fahrenheit converter and vice versa written in C++.
 Comments to explain the code inside.
 Uses the stdio and cmath libraries.
-The c2f file is the compiled binary, the c2f.cpp is the surce code.
+The c2f.o file is the compiled binary, the c2f.cpp is the source code.
+
+USE:
+1. run ./c2f.o (or follow the instructions below to just run c2f.)
+2. type either 'c' or 'f' to decide whether converting celsius to fahrenheit or vice versa.
+3. type the temperatuyre you would like to convert.
+
+To make it a command on Linux,
+open a terminal window.
+run 'cp c2f.o c2f'
+then run 'sudo cp c2f /bin/c2f'
--- c2f.cpp
+++ c2f.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 #include<cmath>
-int main()
-{
-    float celsius, fahrenheit;
 
-    /* Recieve celcius temperature from user */
+void c2f()
+{
+  float celsius, fahrenheit;
+   /* Recieve celcius temperature from user */
     std::cin >> celsius;
 
     /* Calculate the temperature in fahrenheit from celcius */
@@ -12,6 +12,38 @@ int main()
 
     /* Output the temperature in fahrenheit */
     std::cout << round(fahrenheit);
+}
+
+void f2c(){
+  float celsius, fahrenheit;
+  /* Recieve fahrenheit temperature from user */
+    std::cin >> fahrenheit;
+
+    /* Calculate the temperature in celsius from fahrenheit */
+    celsius = (fahrenheit-32)/1.8;
+
+    /* Output the temperature in celsius */
+    std::cout << round(celsius);
+}
+
+int main()
+{
+    char conversion;
+
+std::cout << "to:\n";
+std::cin >> conversion;
+
+if(conversion == 'c'){
+
+c2f();
+
+}else if(conversion == 'f'){
+
+f2c();
+
+}else{
+  std::cout << "Unsuported conversion.\nPlease Try again.\n";
+}
 
     /* Standard housekeeping */
     std::cout << std::endl;
