2d32eba10b7e8690a0994bae5675776c867e9e86
Taemon-Coolz <77027609+Taemon-Coolz@users.noreply.github.com>
2021-11-15 18:01:53 -0500

    Add files via upload

---
+++ c2f.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<cmath>
+
+void c2f()
+{
+  float celsius, fahrenheit;
+   /* Recieve celcius temperature from user */
+    std::cin >> celsius;
+
+    /* Calculate the temperature in fahrenheit from celcius */
+    fahrenheit = (celsius*1.8)+32;
+
+    /* Output the temperature in fahrenheit */
+    std::cout << round(fahrenheit);
+}
+
+void f2c(){
+  float celsius, fahrenheit;
+  /* Recieve fahrenheit temperature from user */
+    std::cin >> fahrenheit;
+
+    /* Calculate the temperature in celsius from fahrenheit */
+    celsius = (fahrenheit-32)/1.8;
+
+    /* Output the temperature in celsius */
+    std::cout << round(celsius);
+}
+
+int main()
+{
+    char conversion;
+
+std::cout << "to:\n";
+std::cin >> conversion;
+
+if(conversion == 'c'){
+
+c2f();
+
+}else if(conversion == 'f'){
+
+f2c();
+
+}else{
+  std::cout << "Unsuported conversion.\nPlease Try again.\n";
+}
+
+    /* Standard housekeeping */
+    std::cout << std::endl;
+    return 0;
+}
